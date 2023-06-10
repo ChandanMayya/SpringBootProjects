13c742daf9edd1b38636e32822ea536b42a584f1
ChandanMayya <chandanmogral@gmail.com>
2023-06-10 13:34:21 +0530

    RoomModification:10/6@1:34PM[2]

--- chatroom/room/src/main/java/com/example/room/controller/RoomController.java
+++ chatroom/room/src/main/java/com/example/room/controller/RoomController.java
@@ -1,2 +1,23 @@
-package com.example.room.controller;public class RoomController {
+package com.example.room.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/room")
+public class RoomController {
+
+    @GetMapping("/list")
+    public String listRooms(){
+        return null;
+    }
+    @GetMapping("/view/{id}")
+    public String viewRoom(){
+        return null;
+    }
+    @GetMapping("/enter/{id}")
+    public String enterRoom(){
+        return null;
+    }
 }
--- chatroom/room/src/main/java/com/example/room/controller/service/RoomService.java
+++ chatroom/room/src/main/java/com/example/room/controller/service/RoomService.java
@@ -1,2 +1,8 @@
-package com.example.room.controller.service;public class RoomService {
+package com.example.room.controller.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+
 }
