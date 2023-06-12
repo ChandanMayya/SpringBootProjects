5c8a924ff2082de117b974d31ebc0b45d4ef8338
ChandanMayya <chandanmogral@gmail.com>
2023-06-12 15:55:07 +0530

    GatewayBase:12-06@03:54PM[3]:CreateGateway

--- chatroom/user/src/main/java/com/example/user/controller/UserController.java
+++ chatroom/user/src/main/java/com/example/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.user.dto.UserDto;
 import com.example.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,9 @@ public class UserController {
         String description = request.getParameter("description");
         UserDto userDto = new UserDto(name,description,new Date());
         service.saveUser(userDto);
+    }
+    @GetMapping("/test")
+    public String  ping(){
+        return "Hi from user endpoint!";
     }
 }
