0e01889d20549b5ffd28b45072494a4c7972c302
ChandanMayya <chandanmogral@gmail.com>
2023-06-12 16:52:40 +0530

    RabitProjBase:12-06@4:52PM[4]:CreatedEmptyBranchForTestsOnRabbit

--- chatroom/message/src/main/java/com/example/message/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.message.repository;
-
-import com.example.message.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message,Integer> {
-}
